0b10ae0497f089e5c9e91e232a7249413811f748
yiixaosir <32405612+yixiaosir@users.noreply.github.com>
2017-11-01 18:59:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const handler = createHandler({ path: '/webhook', secret: 'c111222' })
     // 上面的 secret 保持和 GitHub 后台设置的一致
 
 const port = 9988
-const projects = ['cyx.saigsf.com', 'vnshop', 'deploy', 'de']
+const projects = ['hello.yixiaosir.top', 'vnshop', 'deploy', 'de']
 
 const projectHandler = (event, action) => {
     const project = event.payload.repository.de

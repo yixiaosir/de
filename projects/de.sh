1c16984ccea09b5608d20f53f9f088f8bc0f3032
yiixaosir <32405612+yixiaosir@users.noreply.github.com>
2017-11-03 09:40:23 +0800

    Update de.sh

--- projects/de.sh
+++ projects/de.sh
@@ -1,1 +1,20 @@
 
+#!/bin/bash
+ 
+WEB_PATH='/home/wwwroot/vnshop/'
+WEB_PATH_CLIENT='/home/wwwroot/vnshop/client'
+WEB_USER='www'
+WEB_USERGROUP='www'
+ 
+echo "Start deployment cyx.saigsf.com"
+cd $WEB_PATH
+echo "pulling source code..."
+# git reset --hard origin/release
+# git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+cd $WEB_PATH_CLIENT
+npm run build
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH_CLIENT
+echo "Finished."
